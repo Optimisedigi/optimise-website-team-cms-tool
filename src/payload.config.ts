5ddeba8bb7354e627f1a97cd3b62357bc1fa6b2f
Peter Tu <peter@optimisedigital.online>
2026-05-20 21:54:48 +1000

    revert: disable SaveLoaderToast provider (causes blank screen)

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -99,7 +99,7 @@ export default buildConfig({
       actions: ["./components/NotificationsBell", "./components/UserDisplayName"],
       beforeNavLinks: ["./components/SidebarLogo"],
       afterNavLinks: ["./components/SidebarNavExtras"],
-      providers: ["./components/ViewportMeta", "./components/RocketLoader", "./components/SaveLoaderToast", "./components/FirstLoginSetup", "./components/DashboardGate", "./components/NavigationRecovery", "./components/MiniSidebar", "./components/OptiMateLauncher", "./components/PayloadShiftSelect", "./components/ShowPasswordToggle", "./components/RoleBodyClass"],
+      providers: ["./components/ViewportMeta", "./components/RocketLoader", "./components/FirstLoginSetup", "./components/DashboardGate", "./components/NavigationRecovery", "./components/MiniSidebar", "./components/OptiMateLauncher", "./components/PayloadShiftSelect", "./components/ShowPasswordToggle", "./components/RoleBodyClass"],
       beforeDashboard: ["./components/Dashboard"],
     },
   },
